4861cb6a072cb0ec8deea6daf4be72d9fe99a38c
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:13:13 -0700

    Add files via upload

---
+++ UserAuditableEntityRepository.cs
@@ -0,0 +1,28 @@
+﻿using Penguin.Cms.Entities;
+using Penguin.Messaging.Core;
+using Penguin.Persistence.Abstractions.Interfaces;
+using Penguin.Persistence.Repositories;
+
+namespace Penguin.Cms.Repositories
+{
+    /// <summary>
+    /// This class should accept a user session and update the creator, but it does not currently. 
+    /// </summary>
+    /// <typeparam name="T">Any user auditable entity type</typeparam>
+    public class UserAuditableEntityRepository<T> : AuditableEntityRepository<T> where T : UserAuditableEntity
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of this repository
+        /// </summary>
+        /// <param name="dbContext">The persistence context to use for the obejcts</param>
+        /// <param name="messageBus">And optional message bus for persistence messages</param>
+        public UserAuditableEntityRepository(IPersistenceContext<T> dbContext, MessageBus messageBus = null) : base(dbContext, messageBus)
+        {
+
+        }
+
+        #endregion Constructors
+    }
+}
