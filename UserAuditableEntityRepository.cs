f76f4e9290151dad08fcffd7be56f60ac8b0f07e
anloffredo <loffredo.ap@gmail.com>
2019-09-07 12:21:00 -0700

    Automated cleanup
    Automated cleanup

--- UserAuditableEntityRepository.cs
+++ UserAuditableEntityRepository.cs
@@ -6,13 +6,11 @@ using Penguin.Persistence.Repositories;
 namespace Penguin.Cms.Repositories
 {
     /// <summary>
-    /// This class should accept a user session and update the creator, but it does not currently. 
+    /// This class should accept a user session and update the creator, but it does not currently.
     /// </summary>
     /// <typeparam name="T">Any user auditable entity type</typeparam>
     public class UserAuditableEntityRepository<T> : AuditableEntityRepository<T> where T : UserAuditableEntity
     {
-        #region Constructors
-
         /// <summary>
         /// Creates a new instance of this repository
         /// </summary>
@@ -20,9 +18,6 @@ namespace Penguin.Cms.Repositories
         /// <param name="messageBus">And optional message bus for persistence messages</param>
         public UserAuditableEntityRepository(IPersistenceContext<T> dbContext, MessageBus messageBus = null) : base(dbContext, messageBus)
         {
-
         }
-
-        #endregion Constructors
     }
 }
